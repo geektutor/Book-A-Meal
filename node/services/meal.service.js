bf97588c197139f44dbaec78b5af4d746d0c6a16
Geektutor <sodiq.akinjobi@gmail.com>
2019-02-17 18:05:02 +0100

    Did some ESlint magic

--- node/services/meal.service.js
+++ node/services/meal.service.js
@@ -1,4 +1,5 @@
-import dummyData from '../utils/dummyData';
+/* eslint-disable no-param-reassign */
+import dummyData from '../data/dummyData';
 import Meal from '../models/meal.model';
 
 const MealService = {
@@ -9,11 +10,10 @@ const MealService = {
       newMeal.name = meal.name;
       newMeal.size = meal.size;
       newMeal.price = meal.price;
+      return newMeal;
     });
-
     return validMeals;
   },
-
   addMeal(meal) {
     const mealLength = dummyData.meals.length;
     const lastId = dummyData.meals[mealLength - 1].id;
@@ -22,8 +22,8 @@ const MealService = {
     dummyData.meals.push(meal);
     return meal;
   },
-  getMeal(id) {
-    const meal = dummyData.meals.find(meal => (meal.id = id));
+  getAMeal(id) {
+    const meal = dummyData.meals.find(meal => meal.id === id);
     return meal || {};
   }
 };
