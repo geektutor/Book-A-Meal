04bae459752ac524ce74b945bf1dc349255dd6f1
Geektutor <sodiq.akinjobi@gmail.com>
2019-02-18 08:43:57 +0100

    Added the menu controller and the updated the dummyData with menu json. I also edited the menu routes

--- node/data/dummyData.js
+++ node/data/dummyData.js
@@ -3,38 +3,52 @@ export default {
     {
       id: 1,
       name: 'Iyan',
-      size: 'Medium',
-      price: '450'
+      size: 'Large',
+      price: '450',
+      imageUrl:
+        'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
     },
     {
       id: 2,
       name: 'Iyan',
       size: 'Medium',
-      price: '450'
+      price: '250',
+      imageUrl:
+        'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
     },
     {
       id: 3,
-      name: 'Iyan',
+      name: 'Chicken Salad',
       size: 'Medium',
-      price: '550'
-    },
+      price: '450',
+      imageUrl:
+        'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }
+  ],
+  orders: [
     {
-      id: 4,
-      name: 'Iyan',
-      size: 'Medium',
-      price: '950'
-    },
+      id: 1,
+      date: '18/02/2019',
+      customerId: 1,
+      total: '450',
+      meal: [
+        {
+          name: 'Chicken Salad',
+          size: 'Medium'
+        }
+      ],
+      delivered: true
+    }
+  ],
+  menu: [
     {
-      id: 5,
-      name: 'Iyan',
-      size: 'Medium',
-      price: 150
-    },
-    {
-      id: 6,
-      name: 'Iyan',
-      size: 'Medium',
-      price: 350
+      date: '18-02-2019',
+      meal: [
+        {
+          name: 'Chicken Salad',
+          size: 'Medium'
+        }
+      ]
     }
   ]
 };
--- node/routes.js
+++ node/routes.js
@@ -2,11 +2,19 @@ import { Router } from 'express';
 
 // controller
 import MealController from './controllers/meal.controller';
+import MenuController from './controllers/menu.controller';
 
 const router = Router();
 
-router.get('/', MealController.fetchAllMeals);
-router.post('/', MealController.addAMeal);
-router.get('/:id', MealController.getSingleMeal);
+router.get('/meals/', MealController.fetchAllMeals);
+
+router.post('/meals/', MealController.addAMeal);
+
+router.get('/meals/:id', MealController.getSingleMeal);
+
+// add menu controller
+router.get('/menu/', MenuController.fetchAllMenus);
+
+router.post('/menu/', MenuController.addAMenu);
 
 export default router;
